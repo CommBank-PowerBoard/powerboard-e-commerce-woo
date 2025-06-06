9344a04d6c8d30e8807bc1a6ab563fa09732e5f3
AndriiYeshchenko <andrii.yeshchenko@paydock.com>
2025-06-06 15:58:50 +0300

    JS assets

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'e1154a8cbbaab3847a16');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '6a24293ded1355154753');
