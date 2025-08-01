a4bc435e35347ea4f15ba5ee630bdfe9258ba345
Manuel Mesquita <manuel.mesquita@paydock.com>
2025-08-01 14:45:44 +0100

    build wooommerce-block code

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '74f27327ac368cbeac1b');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '4e7e89c7e6ab256c048c');
