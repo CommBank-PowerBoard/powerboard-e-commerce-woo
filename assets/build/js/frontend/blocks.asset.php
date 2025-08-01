90983bec4bd98a741f6cd78ebc1751444b5ebc24
Manuel Mesquita <manuel.mesquita@paydock.com>
2025-08-01 14:15:49 +0100

    rebuild files for woocommerce-blocks

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '2f9d0933e3f6c8961133');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '7f1a560d86e86f10c952');
