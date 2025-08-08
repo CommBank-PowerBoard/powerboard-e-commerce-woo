c2c180f620e2cd0daf9a74bd9a10258123ad1c3d
Andrii Yes <andrii.yeshchenko@paydock.com>
2025-08-08 20:29:37 +0300

    No shipping templates (ECOMM-309)

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '2cb5fd2754e5c46479ff');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'e9374d2c67bab2b191ab');
