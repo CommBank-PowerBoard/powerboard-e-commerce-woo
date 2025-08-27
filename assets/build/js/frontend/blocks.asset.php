d504ef01ebebb55d236e0b58e75af0e3efdc80b5
Manuel Mesquita <manuel.mesquita@paydock.com>
2025-08-27 15:55:59 +0100

    re-build all blocks code

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '254da2f858764475d8fe');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'd1a13a1ad75671b276c4');
