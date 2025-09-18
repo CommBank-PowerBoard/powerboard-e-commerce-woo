841b6ee680499693a4f340e6408fcb7ed224a628
FabianaBarros <fabiana.barros@paydock.com>
2025-09-18 10:14:17 +0100

    fix: fix payment methods being displayed without being enabled for version 2

--- includes/Helpers/AvailablePaymentMethodsHelper.php
+++ includes/Helpers/AvailablePaymentMethodsHelper.php
@@ -16,28 +16,37 @@ class AvailablePaymentMethodsHelper {
 
 		if ( empty( $result['error'] ) ) {
 			$configuration_template_content = JsonHelper::decode_stringified_json( $result['resource']['data']['content'] );
-			$payment_methods_keys           = array_keys( $configuration_template_content['payment_method_options'] );
+			$configuration_template_version = $result['resource']['data']['version'];
+			$payment_method_options         = $configuration_template_content['payment_method_options'];
 
-			foreach ( $payment_methods_keys as $key ) {
-				switch ( $key ) {
-					case 'card':
-						$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::CARD );
-						break;
-					case 'afterpay_checkout':
-						$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::AFTERPAY );
-						break;
-					case 'applepay_wallet':
-						$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::APPLE_PAY );
-						break;
-					case 'googlepay_wallet':
-						$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::GOOGLE_PAY );
-						break;
-					case 'paypal_wallet':
-						$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::PAYPAL );
-						break;
-					case 'zip_checkout':
-						$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::ZIP );
-						break;
+			if ( !empty( $payment_method_options ) ) {
+				$payment_methods_keys = array_keys( $payment_method_options );
+
+				foreach ( $payment_methods_keys as $key ) {
+					if ( $configuration_template_version > 1 && $payment_method_options[ $key ]['enabled'] === false ) {
+						continue;
+					}
+
+					switch ( $key ) {
+						case 'card':
+							$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::CARD );
+							break;
+						case 'afterpay_checkout':
+							$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::AFTERPAY );
+							break;
+						case 'applepay_wallet':
+							$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::APPLE_PAY );
+							break;
+						case 'googlepay_wallet':
+							$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::GOOGLE_PAY );
+							break;
+						case 'paypal_wallet':
+							$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::PAYPAL );
+							break;
+						case 'zip_checkout':
+							$payment_methods = array_merge( $payment_methods, AvailablePaymentMethodsEnum::ZIP );
+							break;
+					}
 				}
 			}
 		}
