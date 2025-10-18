dbe021ff949558621b2616b6d9b5c9902a7f3f77
Manuel Mesquita <manuel.mesquita@paydock.com>
2025-10-18 22:39:18 +0100

    ECOMM-536 solving reformat errors

--- includes/Helpers/Util/PaymentGatewayHelper.php
+++ includes/Helpers/Util/PaymentGatewayHelper.php
@@ -9,10 +9,10 @@ use PowerBoard\Enums\ConfigAPIEnum;
 use PowerBoard\Services\PaymentGateway\MasterWidgetPaymentService;
 
 class PaymentGatewayHelper {
-	
-	public static function get_payment_gateway (): ?MasterWidgetPaymentService {
+
+	public static function get_payment_gateway(): ?MasterWidgetPaymentService {
 		$payment_gateways = WC()->payment_gateways->get_available_payment_gateways();
-		
+
 		if ( isset( $payment_gateways[ POWER_BOARD_PLUGIN_PREFIX ] ) ) {
 			return $payment_gateways[ POWER_BOARD_PLUGIN_PREFIX ];
 		} else {
@@ -20,42 +20,42 @@ class PaymentGatewayHelper {
 			return null;
 		}
 	}
-	
-	
+
+
 	/**
 	 * @param string $http_host
 	 * @return array|false|int|mixed|string|null
 	 */
-	private static function standardize_url ( $http_host ) {
-		
+	private static function standardize_url( $http_host ) {
+
 		if ( !is_string( $http_host ) || empty( $http_host ) ) {
 			return null;
 		}
-		
+
 		$http_host = str_replace( 'http://', '', $http_host );
 		$http_host = str_replace( 'https://', '', $http_host );
 		$http_host = wp_parse_url( 'https://' . $http_host, PHP_URL_HOST );
-		
+
 		return $http_host;
-		
+
 	}
-	
-	
+
+
 	/**
 	 * @param $http_host
 	 * @return bool
 	 */
-	public static function is_valid_domain_name ( $http_host ): bool {
-		
+	public static function is_valid_domain_name( $http_host ): bool {
+
 		$http_host = self::standardize_url( $http_host );
-		
+
 		if ( empty( $http_host ) ) {
 			return false;
 		}
-		
+
 		return str_ends_with( $http_host, ConfigAPIEnum::DOMAIN_API );
-		
+
 	}
-	
-	
+
+
 }
