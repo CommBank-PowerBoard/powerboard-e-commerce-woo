aa4f5615e53e0f906dd74bf424ef4dc0b16bbadc
Manuel Mesquita <manuel.mesquita@paydock.com>
2025-10-22 13:36:29 +0100

    ECOMM-505 fixing unit-tests

--- tests/unit/src/Services/BaseServiceTest.php
+++ tests/unit/src/Services/BaseServiceTest.php
@@ -101,17 +101,4 @@ class BaseServiceTest extends TestCase {
 	public function StandardTest(): void {
 		$this->assertEquals( 1, 1 );
 	}
-
-    /**
-     * Test that invalid nonces are rejected
-     *
-     * @test
-     */
-    public function StandardTest(): void
-    {
-        $this->assertEquals( 1, 1 );
-
-    }
-
-
 }
